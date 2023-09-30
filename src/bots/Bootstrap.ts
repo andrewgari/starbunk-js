dafe63997a4bd5f0b55a5165bb5827694a2a1aa2
Andrew Gari <covadax.ag@gmail.com>
2023-09-30 22:29:54 +0000

    added dynamic bot imports

--- src/bots/Bootstrap.ts
+++ src/bots/Bootstrap.ts
@@ -1,23 +1,70 @@
-import { Client } from 'discord.js';
 import ReplyBot from './ReplyBot';
-import BlueBot from './reply-bots/Bluebot';
-import Botbot from './reply-bots/Botbot';
-import MacaroniBot from './reply-bots/MacaroniBot';
-import GundamBot from './reply-bots/GundamBot';
-import HoldBot from './reply-bots/HoldBot';
-import ChaosBot from './reply-bots/ChaosBos';
+import Client from '../discord/DiscordClient';
+import { readdirSync } from 'fs';
+import { Events, REST, Routes } from 'discord.js';
 
-const bots = new Map<string, ReplyBot>();
-
-export const registerBot = (bot: ReplyBot) => {
-  bots.set(bot.getBotName(), bot);
+const registerBots = async (client: Client) => {
+  readdirSync(`./src/bots/reply-bots`).forEach(async (file) => {
+    const fileName = file.replace('.ts', '');
+    await import(`./reply-bots/${fileName}`).then((botClass) => {
+      if (!botClass) return;
+      const bot = new botClass.default(client) as ReplyBot;
+      if (
+        !bot ||
+        !bot.getBotName() ||
+        client.bots.find((b) => b.getBotName() === bot.getBotName())
+      )
+        return;
+      client.bots.set(bot.getBotName(), bot);
+      console.log(`Registered Bot: ${bot.getBotName()}`);
+    });
+  });
 };
 
-export default (client: Client): void => {
-  registerBot(new BlueBot(client));
-  registerBot(new Botbot(client));
-  registerBot(new MacaroniBot(client));
-  registerBot(new GundamBot(client));
-  registerBot(new HoldBot(client));
-  registerBot(new ChaosBot(client));
+const registerCommands = async (
+  client: Client,
+  clientId: string,
+  guildId: string
+) => {
+  readdirSync(`./src/commands/`).forEach(async (file) => {
+    const fileName = file.replace('.ts', '');
+    await import(`../commands/${fileName}`).then((cmd) => {
+      const command = cmd.default;
+      if (
+        !command ||
+        !command?.data?.name ||
+        client.commands.find((c) => c.data.name === command.data.name)
+      )
+        return;
+      client.commands.set(command.data.name, command);
+      console.log(`Registered Command: ${command.data.name}`);
+    });
+  });
+  console.log('creating rest client');
+  const rest = new REST({ version: '10' }).setToken(client.token!);
+  console.log('created rest client');
+
+  try {
+    console.log('Registering slash commands');
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: client.commands
+    });
+    console.log('Registered Commands');
+  } catch (e) {
+    console.error(e);
+  }
+
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    interaction.reply('Got it');
+  });
+};
+
+export default async (
+  client: Client,
+  clientId: string,
+  guildId: string
+): Promise<void> => {
+  await registerBots(client);
+  await registerCommands(client, clientId, guildId);
 };
