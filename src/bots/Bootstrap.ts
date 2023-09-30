8e6998720ed2250f362b2eecf3813addd36fc94c
Andrew Gari <35663269+andrewgari@users.noreply.github.com>
2023-09-29 20:57:28 -0400

    macaoni (#14)

--- src/bots/Bootstrap.ts
+++ src/bots/Bootstrap.ts
@@ -2,6 +2,7 @@ import { Client } from 'discord.js';
 import ReplyBot from './ReplyBot';
 import BlueBot from './reply-bots/Bluebot';
 import Botbot from './reply-bots/Botbot';
+import MacaroniBot from './reply-bots/MacaroniBot';
 import GundamBot from './reply-bots/GundamBot';
 import HoldBot from './reply-bots/HoldBot';
 
@@ -14,6 +15,7 @@ export const registerBot = (bot: ReplyBot) => {
 export default (client: Client): void => {
   registerBot(new BlueBot(client));
   registerBot(new Botbot(client));
+  registerBot(new MacaroniBot(client));
   registerBot(new GundamBot(client));
   registerBot(new HoldBot(client));
 };
