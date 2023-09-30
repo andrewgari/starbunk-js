40e33e13f7e0720fabde1e4c2928bd18cb93ce65
Andrew Gari <covadax.ag@gmail.com>
2023-09-30 00:52:02 +0000

    macaoni

---
+++ src/bots/reply-bots/MacaroniBot.ts
@@ -0,0 +1,34 @@
+import { Message, TextChannel } from 'discord.js';
+import { ReplyBot } from '../ReplyBot';
+
+export default class MacaroniBot extends ReplyBot {
+  private readonly botName = 'MacaroniBot';
+  private readonly avatarUrl = 'https://i.imgur.com/fgbH6Xf.jpg';
+  private readonly vennPattern = /\bvenn\b/;
+  private readonly macaorniPattern = /\bmacaroni\b/;
+  private readonly macaroniNamePattern =
+    /venn(?!.*Tyrone "The "Macaroni" Man" Johnson" Caelum).*/;
+  private readonly vennResponse = `Correction: you mean Venn "Tyrone "The "Macaroni" Man" Johnson" Caelum`;
+  private readonly macaroniResponse = `Are you trying to reach <@&%s>`;
+  private readonly vennID = '151120340343455744';
+
+  getBotName(): string {
+    return this.botName;
+  }
+  getAvatarUrl(): string {
+    return this.avatarUrl;
+  }
+  handleMessage(message: Message<boolean>): void {
+    if (
+      message.content.match(this.macaorniPattern) &&
+      !message.content.match(this.macaroniNamePattern)
+    ) {
+      this.sendReply(message.channel as TextChannel, this.vennResponse);
+    } else if (message.content.match(this.vennPattern)) {
+      this.sendReply(
+        message.channel as TextChannel,
+        this.macaroniResponse.replace('%s', this.vennID)
+      );
+    }
+  }
+}
