a091405d0e0aed346724c3674ba85f3d227f3a8a
Matthew Hassenboehler <mcjhassenboehler@gmail.com>
2023-10-05 17:26:31 -0500

    SheeshBot (#43)
    Co-authored-by: Andrew Gari <covadax.ag@gmail.com>

---
+++ src/bots/reply-bots/SheeshBot.ts
@@ -0,0 +1,30 @@
+import { Message, TextChannel } from 'discord.js';
+import ReplyBot from '../ReplyBot';
+import UserID from '../../discord/UserID';
+
+export default class SheeshBot extends ReplyBot {
+  private botName = 'SheeshBot';
+  private readonly avatarUrl = 'https://i.imgflip.com/5fc2iz.png?a471000';
+
+  private readonly defaultPattern = /\bshee+sh\b/;
+
+  generateRandomEs(): string {
+    const numberOfEs = Math.floor(Math.random() * 10);
+    return 'e'.repeat(numberOfEs);
+  }
+
+  getBotName(): string {
+    return this.botName;
+  }
+  getAvatarUrl(): string {
+    return this.avatarUrl;
+  }
+  response(): string {
+    return 'Sh' + this.generateRandomEs() + 'sh!';
+  }
+
+  handleMessage(message: Message<boolean>): void {
+    if (message.content.match(this.defaultPattern))
+      this.sendReply(message.channel as TextChannel, this.response());
+  }
+}
