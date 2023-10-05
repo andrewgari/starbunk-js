949d4aa221b30c4586c906161f5e3b00af08c2e0
Andrew Gari <35663269+andrewgari@users.noreply.github.com>
2023-10-05 18:55:17 -0400

    fixed holdbot (#46)

--- src/bots/reply-bots/HoldBot.ts
+++ src/bots/reply-bots/HoldBot.ts
@@ -1,10 +1,11 @@
 import { Message, TextChannel } from 'discord.js';
 import ReplyBot from '../ReplyBot';
+import isSelf from '../../utils/isSelf';
 
 export default class HoldBot extends ReplyBot {
   private readonly botName = 'HoldBot';
   private readonly avatarUrl = 'https://i.imgur.com/YPFGEzM.png';
-  private readonly pattern = /^Hold\.?$/;
+  private readonly pattern = /^Hold\.?$/i;
   private readonly response = `Hold.`;
 
   getBotName(): string {
@@ -14,7 +15,10 @@ export default class HoldBot extends ReplyBot {
     return this.avatarUrl;
   }
   handleMessage(message: Message<boolean>): void {
-    if (message.content.match(this.pattern)) {
+    if (
+      isSelf(message, this.getBotName()) &&
+      message.content.match(this.pattern)
+    ) {
       this.sendReply(message.channel as TextChannel, this.response);
     }
   }
