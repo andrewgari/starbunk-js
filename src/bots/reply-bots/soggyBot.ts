9c11556f52b123c6a1763cbc7ccda92f6af1d768
Andrew Gari <covadax.ag@gmail.com>
2023-10-06 06:37:43 +0000

    da wet bread

---
+++ src/bots/reply-bots/soggyBot.ts
@@ -0,0 +1,25 @@
+import { Message, TextChannel } from 'discord.js';
+import ReplyBot from '../replyBot';
+import roleIDs from 'src/discord/roleIDs';
+
+export default class SoggyBot extends ReplyBot {
+  private readonly botName = 'SoggyBot';
+  private readonly avatarUrl = `https://imgur.com/OCB6i4x.jpg`;
+  private readonly pattern = /wet bread/i;
+  private readonly response = `Sounds like somebody enjoys wet bread`;
+
+  getBotName(): string {
+    return this.botName;
+  }
+  getAvatarUrl(): string {
+    return this.avatarUrl;
+  }
+  handleMessage(message: Message<boolean>): void {
+    if (
+      message.content.match(this.pattern) &&
+      message.member?.roles.cache.some((role) => role.id === roleIDs.WetBread)
+    ) {
+      this.sendReply(message.channel as TextChannel, this.response);
+    }
+  }
+}
