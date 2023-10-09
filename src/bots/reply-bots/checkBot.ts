bc142c04e89fcc433b72e7aa0f735497315db22c
mcjhassenboehler <Mcjhassenboehler@gmail.com>
2023-10-09 13:52:02 -0500

    CheckBot created

---
+++ src/bots/reply-bots/checkBot.ts
@@ -0,0 +1,24 @@
+import { Message, TextChannel } from 'discord.js';
+import ReplyBot from '../replyBot';
+import isSelf from '../../utils/isSelf';
+
+export default class BabyBot extends ReplyBot {
+  private readonly botName = 'CheckBot';
+  private readonly avatarUrl = `https://m.media-amazon.com/images/I/21Unzn9U8sL._AC_.jpg`;
+  private readonly pattern = /\bczech\b/i;
+  private readonly response = "I believe you mean 'check'.";
+
+  getBotName(): string {
+    return this.botName;
+  }
+  getAvatarUrl(): string {
+    return this.avatarUrl;
+  }
+  handleMessage(message: Message<boolean>): void {
+    if (isSelf(message, this.getBotName())) return;
+
+    if (message.content.match(this.pattern)) {
+      this.sendReply(message.channel as TextChannel, this.response);
+    }
+  }
+}
