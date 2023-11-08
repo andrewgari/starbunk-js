c8b6a66d6bf21e2ddee9b683b1c584a0f0335015
Andrew Gari <covadax.ag@gmail.com>
2023-11-08 22:09:15 +0000

    it was going buck wild

--- src/starbunk/bots/reply-bots/sigGreatBot.ts
+++ src/starbunk/bots/reply-bots/sigGreatBot.ts
@@ -2,20 +2,25 @@ import { Message, TextChannel } from 'discord.js';
 import ReplyBot from '../replyBot';
 
 export default class SigBestBot extends ReplyBot {
-  botName: string = '';
-  avatarUrl: string = '';
+  botName: string = 'SigBestBot';
+  avatarUrl: string = 'PickleSig.comOrWhatever';
   readonly pattern = /^(?=.*\bsig(g?les)?\b)(?=.*best\b).*$/gim;
 
   getBotName(): string {
     return this.botName;
   }
+
   getAvatarUrl(): string {
     return this.avatarUrl;
   }
+
   getResponse(): string {
-    return 'Man, Sig really is the best :nodders:';
+    return 'Man, Sig really is the best.';
   }
+
   handleMessage(message: Message): void {
+    if (message.author.bot) return;
+
     if (message.content.match(this.pattern)) {
       this.botName = message.author.displayName;
       this.avatarUrl =
