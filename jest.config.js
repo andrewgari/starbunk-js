ed2b1024424f3c1227d1bd3f5278c485e761fbf5
andrewgari <covadax.ag@gmail.com>
2025-02-20 22:13:32 -0500

    chore: Fix test dependencies and configuration

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  }
+}; 
