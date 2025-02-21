a1c748252e4ccfa674383be45d443f55c29dd47b
andrewgari <covadax.ag@gmail.com>
2025-02-21 11:53:50 -0500

    chore: configure eslint and prettier with consistent formatting rules

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+export default {
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	printWidth: 120,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	endOfLine: 'lf'
+}; 
