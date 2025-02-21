deeec6b0e51c013bf7b3bdd9312923da55629311
andrewgari <covadax.ag@gmail.com>
2025-02-21 00:42:40 -0500

    oxford commas

--- src/discord/guildIDs.ts
+++ src/discord/guildIDs.ts
@@ -1,5 +1,5 @@
 export default {
-  StarbunkCrusaders: '753251582719688714',
-  Snowfall: '696689954759245915',
-  CovaDaxServer: '798613445301633134'
+	StarbunkCrusaders: '753251582719688714',
+	Snowfall: '696689954759245915',
+	CovaDaxServer: '798613445301633134',
 };
