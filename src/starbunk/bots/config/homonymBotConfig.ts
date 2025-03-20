1f7372926b8fe8d4d722edbad713e7213abef567
andrewgari <covadax.ag@gmail.com>
2025-03-19 20:06:24 -0400

    Updates homonym bot configuration
    Changes default avatar URL to a new image link.
    Adds additional homonym pairs, enhancing bot's correction capabilities.

--- src/starbunk/bots/config/homonymBotConfig.ts
+++ src/starbunk/bots/config/homonymBotConfig.ts
@@ -7,7 +7,7 @@ export class HomonymBotConfig {
 	static readonly Name = 'HomonymBot';
 
 	static readonly Avatars = {
-		Default: 'https://i.imgur.com/mWVgriz.png'
+		Default: 'https://imgur.com/a/0X11DGL'
 	};
 
 	static readonly HomonymPairs: HomonymPair[] = [
@@ -24,6 +24,29 @@ export class HomonymBotConfig {
 			corrections: {
 				"your": "you're",
 				"you're": "your"
+			}
+		},
+		{
+			words: ["where", "wear"],
+			corrections: {
+				"where": "wear",
+				"wear": "where"
+			}
+		},
+		// bored, board
+		{
+			words: ["bored", "board"],
+			corrections: {
+				"bored": "board",
+				"board": "bored"
+			}
+		},
+		{
+			words: ["bored", "board"],
+			corrections: {
+				"sense": "cents",
+				"cents": "sense",
+				"scent": "sense"
 			}
 		},
 		{
