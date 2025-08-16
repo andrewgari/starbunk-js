b8cb9410e72463047a179298639e474f4b78e286
Andrew Gari <covadax.ag@gmail.com>
2025-08-16 14:22:47 -0400

    bunkbot: use regex pattern from constants via matchesPattern for ClankerBot trigger

--- containers/bunkbot/src/reply-bots/clanker-bot/triggers.ts
+++ containers/bunkbot/src/reply-bots/clanker-bot/triggers.ts
@@ -1,10 +1,10 @@
 import { createTriggerResponse } from '../../core/trigger-response';
-import { containsWord } from '../../core/conditions';
-import { CLANKER_BOT_RESPONSES } from './constants';
+import { matchesPattern } from '../../core/conditions';
+import { CLANKER_BOT_RESPONSES, CLANKER_BOT_PATTERNS } from './constants';
 
 export const clankerTrigger = createTriggerResponse({
 	name: 'clanker-word-trigger',
 	priority: 1,
-	condition: containsWord('clanker'),
+	condition: matchesPattern(CLANKER_BOT_PATTERNS.ClankerWord),
 	response: () => CLANKER_BOT_RESPONSES[Math.floor(Math.random() * CLANKER_BOT_RESPONSES.length)],
 });
