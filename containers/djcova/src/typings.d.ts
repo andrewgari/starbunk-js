ff9b5aaac36b6403cab934c1cc4843896937896e
Andrew Gari <35663269+andrewgari@users.noreply.github.com>
2025-09-03 14:21:29 -0400

    feat(djcova): add attachment support to play command (#285)

---
+++ containers/djcova/src/typings.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+declare module '@discordjs/voice';
+declare module '@distube/ytdl-core';
+declare module 'discord.js';
+declare module '../utils/voiceUtils';
+declare module 'fs';
+declare module 'path';
+declare module 'stream';
+declare const process: any;
+declare const require: any;
+declare const __dirname: string;
+declare const fetch: any;
+declare const ReadableStream: any;
