a86be875eeb9932ed68eaa082302e429276f9332
Andrew Gari <covadax.ag@gmail.com>
2026-01-22 20:30:54 -0500

    Revert "chore: consolidate nested config folders into cleaner structure (#489)"
    This reverts commit 4c23bcf04506837a4843b7e9c804a042879a3105.

---
+++ config/release.config.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  branches: [
+    "main",
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        // Write changelog into docs/ so it doesn't violate root-directory cleanliness checks
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        // Persist the next release version to VERSION and sync it into package.json/workspaces
+        prepareCmd:
+          "echo ${nextRelease.version} > config/VERSION && bash scripts/sync-versions.sh",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        // Commit the synced version files and the docs changelog
+        assets: ["package.json", "config/VERSION", "docs/CHANGELOG.md"],
+        message:
+          "chore(release): v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [],
+      },
+    ],
+  ],
+};
+
